1e65330c2e949550c793d74d736734fc398411a1
Leeryeong Song <86554429+leeryeongsong@users.noreply.github.com>
2021-07-23 23:24:12 +0900

    Update level4-3052-나머지.py

--- step5/level4-3052-나머지.py
+++ step5/level4-3052-나머지.py
@@ -7,3 +7,20 @@ for i in range(10):
 
 a_set = set(a)
 print(len(a_set))
+
+
+# 순서가 중요할 경우, 반복문을 사용한다.
+"""
+a = []
+new_a = []
+
+for i in range(10):
+  a.append(int(input()))
+  a[i] = a[i]%42
+
+for j in a:
+  if j not in new_a:
+    new_a.append(j)
+
+print(len(new_a))
+"""
