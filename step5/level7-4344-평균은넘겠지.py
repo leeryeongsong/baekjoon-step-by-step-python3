a4f0787f3188fbd5375a8cbb90fd7a1b1e8d324a
Leeryeong Song <86554429+leeryeongsong@users.noreply.github.com>
2021-07-26 02:02:34 +0900

    Update level7-4344-평균은넘겠지.py

--- step5/level7-4344-평균은넘겠지.py
+++ step5/level7-4344-평균은넘겠지.py
@@ -12,4 +12,4 @@ for i in range(C):
     over_average_student=len(over_average_list)/N[0]*100
     print(f"{over_average_student:.3f}%") # 문자열 포매팅 방법 f-string으로 소수점 자리수 조절하기
     # print("{:.3f}%".format(over_average_student)) # 문자열 포매팅 방법 str.format으로 소수점 자리수 조절하기
-    # print(f"{round(over_average_student, 3)}%") # 이 문제에서는 round() 함수 사용 불가. 소수점 3자리수로 설정해도 40.0%으로 결과 나온다.
+    # print(f"{round(over_average_student, 3)}%") # 이 문제에서는 round() 함수 사용 불가. 40.000%가 아니라 40.0%으로 결과 나온다.
