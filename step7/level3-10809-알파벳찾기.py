369a9bcda99281307f3191d29d112c5716f9229e
Leeryeong Song <86554429+leeryeongsong@users.noreply.github.com>
2021-07-28 15:44:21 +0900

    Update level3-10809-알파벳찾기.py

--- step7/level3-10809-알파벳찾기.py
+++ step7/level3-10809-알파벳찾기.py
@@ -1,5 +1,5 @@
 # https://www.acmicpc.net/problem/10809
-
+"""
 S = input()
 
 for i in range(97, 123):
@@ -9,3 +9,18 @@ for i in range(97, 123):
         if chr(i)==S[j]:
             print(j, end = ' ')
             break
+# 메모리 29200KB, 시간 80ms 코드 길이 193B
+"""
+
+S = input()
+
+for i in range(97, 123):
+    if chr(i) in S:
+        for j in range(len(S)):
+            if chr(i)==S[j]:
+                print(j, end = ' ')
+                break
+    else:
+        print(-1, end = ' ')
+
+# 메모리 29200KB, 시간 80ms 코드 길이 215B   
